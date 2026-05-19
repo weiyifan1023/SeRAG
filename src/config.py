14fbcfd63c45886f635cf00252c49e78df3dc9ed
Yifan Wei <weiyifan21@mails.ucas.ac.cn>
2026-05-19 12:31:44 +0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,22 +1,19 @@
 from dataclasses import dataclass
 from typing import Union
-from src.utils import LLM_Model, ByteDance_OpenAI
-
-
-
+from src.utils import LLM_Model
 
  
 @dataclass
 class SeRAGConfig:
     dataset_name: str
     embedding_model: str = "all-mpnet-base-v2"
-    llm_model: Union[LLM_Model, ByteDance_OpenAI] = None
+    llm_model: LLM_Model = None
     llm_name: str = "gpt-4o-mini"
     chunk_token_size: int = 1000
     spacy_model: str = "en_core_web_trf"
     working_dir: str = "./import"
     batch_size: int = 128
-    max_workers: int = 5
+    max_workers: int = 16
     retrieval_top_k: int = 3
     k_dim: int = 2
     gamma_coarse: float = 0.4
@@ -29,5 +26,4 @@ class SeRAGConfig:
     semantic_k: int = 20
     distance_sigma: int = 5
     min_common_entities: int = 1
-
 
